1292b00bb533dd3b4e779914667e2341c756feb7
Alexandr <124583216+Little5quad@users.noreply.github.com>
2025-03-14 12:04:34 +0200

    Update main.cpp

--- ProcessingListOfObjects/main.cpp
+++ ProcessingListOfObjects/main.cpp
@@ -1,15 +1,30 @@
 #include <iostream>
+#include <string>
 
 #include "input_reader.h"
+#include "group.h"
 
-using std::filesystem::path;
-
-path operator""_p(const char* data, std::size_t sz) {
-	return path(data, data + sz);
-}
+using namespace std::literals;
 
 int main()
 {
-	std::cout << "Hello CMake." << std::endl;
+	//Устраняет проблему с ру буквами в VS
+	//std::setlocale(LC_ALL, "ru_RU.UTF-8");
+	setlocale(LC_ALL, "Russian");
+	//SetConsoleOutputCP(1251);
+	//SetConsoleCP(1251);
+
+	std::string name_file;
+	std::cout << "Input file: "sv;
+	std::getline(std::cin, name_file);
+
+	ManagerGroup mg;
+
+	Load(name_file, mg);
+
+	std::cout << name_file;
+
+	mg.PrintAllObjects();
+
 	return 0;
 }
