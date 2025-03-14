6a42da12d9ae1a6023aaf8c239cf941e85a693c9
Alexandr <124583216+Little5quad@users.noreply.github.com>
2025-03-14 15:09:00 +0200

    Add functionals to output

--- ProcessingListOfObjects/input_reader.h
+++ ProcessingListOfObjects/input_reader.h
@@ -5,15 +5,23 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <deque>
 
 #include "object.h"
 #include "group.h"
+#include "geo.h"
 
 class ParsingError : public std::runtime_error {
 public:
     using runtime_error::runtime_error;
 };
 
+std::ostream& operator<<(std::ostream& out, const Coordinates& coord);
+
+std::ostream& operator<<(std::ostream& out, const Object& obj);
+
 std::vector<std::string> SplitToWords(const std::string& text);
 
 void Load(std::istream& input, ManagerGroup& mg);
+
+void PrintToFile(const std::string& input, ManagerGroup& mg);
